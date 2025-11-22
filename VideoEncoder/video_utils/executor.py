6e8495d263aa70293d735ecf2f53103449d5ddef
Lokesh <23ca1031@mvn.edu.in>
2025-11-22 21:12:21 +0530

    Update executor.py

--- VideoEncoder/video_utils/executor.py
+++ VideoEncoder/video_utils/executor.py
@@ -1,18 +1,18 @@
 from __future__ import annotations
-from aiofiles import open as aiopen
-from aiofiles.os import path as aiopath, makedirs, listdir
-from aioshutil import move
+
 from ast import literal_eval
-from asyncio import create_subprocess_exec, sleep, gather, Event
-from hashlib import sha1
-from asyncio.subprocess import PIPE
-from natsort import natsorted
-from os import path as ospath, walk
+from asyncio import Event
 from time import time
 
-
 async def get_metavideo(video_file):
-    stdout, stderr, rcode = await cmd_exec(['ffprobe', '-hide_banner', '-print_format', 'json', '-show_format', '-show_streams', video_file])
+    stdout, stderr, rcode = await cmd_exec([
+        'ffprobe',
+        '-hide_banner',
+        '-print_format', 'json',
+        '-show_format',
+        '-show_streams',
+        video_file
+    ])
     if rcode != 0:
         LOGGER.error(stderr)
         return {}, {}
@@ -34,7 +34,12 @@ class VidEcxecutor(FFProgress):
         self._gid = gid
         self._start_time = time()
         self._files = []
-        self._qual = {'1080p': '1920', '720p': '1280', '540p': '960', '480p': '854', '360p': '640'}
+        self._qual = {
+            '1080p': '1920',
+            '720p': '1280',
+            '540p': '960',
+            '480p': '854',
+            '360p': '640',
+        }
         super().__init__()
         self.is_cancel = False
-
